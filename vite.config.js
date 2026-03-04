ba4628aa70c40e9965d5900d633c6395573859ed
PramudiWalakuluarachchi01 <pramudinethmini01@gmail.com>
2026-03-04 13:35:18 +0530

    `Update homepage URL in package.json and vite.config.js`

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Pramudi_Portfolio/',
+  base: '/My_Portfolio/',
 })
